90d3c3b10d618cad401ff8c10e33accbfc5bf524
mcgegan <mcgegan@gmail.com>
2025-02-12 11:33:48 +0900

    application : add bottom navigation bar

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -34,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<int> _numbers = [];
 
   void _incrementCounter() {
     setState(() {
@@ -42,38 +41,41 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _numbers = List.generate(100, (index) => index);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         leading: Builder(
-          // Iconbutton을 build로 묶어서 contex 전달하지 않으면 실행 되지 않음
-          builder: (context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {
-                debugPrint('Menu button pressed');
-                //Scaffold.of(context).openDrawer();
-                _showSnackBar(context);
-              },
-            );
-          }
-        ),
+            // Iconbutton을 build로 묶어서 contex 전달하지 않으면 실행 되지 않음
+            builder: (context) {
+          return IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              debugPrint('Menu button pressed');
+              //Scaffold.of(context).openDrawer();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Menu button pressed'),
+                ),
+              );
+            },
+          );
+        }),
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: HomeScreen(counter: _counter, numbers: _numbers),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Settings'),
+        ],
       ),
       drawer: Drawer(
         child: ListView(
@@ -94,11 +96,46 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
 
-  void _showSnackBar(BuildContext context) {
-    final snackBar = SnackBar(
-      content: Text('Hello'),
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+    required int counter,
+    required List<int> numbers,
+  })  : _counter = counter,
+        _numbers = numbers;
+
+  final int _counter;
+  final List<int> _numbers;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          //ListView는 높이 제한 할수 있는 위젯에 넣어야 함
+          //SizeBox에 height 속성 부여해서 사용 가능
+          //Expanded 위젯을 사용하면 남은 스크린 이용해서 높이 제한 없이 사용 가능
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: _numbers.length,
+              itemBuilder: (context, index) {
+                return Text(_numbers[index].toString());
+              },
+            ),
+          ),
+        ],
+      ),
     );
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
